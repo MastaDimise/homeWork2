503334c4a2554c488e2d61c919006b5e9447b8d2
MastaDimise <dmitriyrakaev@gmail.com>
2025-08-26 17:00:10 +0300

    Add files via upload

--- homeWork2/src/Main.java
+++ homeWork2/src/Main.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
 import ru.netology.service.CustomsService;
-
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -8,7 +7,7 @@ public class Main {
         int price = scanner.nextInt();
         System.out.print("Введите вес товара (в кг):");
         int weight = scanner.nextInt();
-        final int TAX = CustomsService.calculateService(price, weight);
-        System.out.print("Размер пошлины в рублях составит: " + TAX + " руб.");
+        int tax = CustomsService.calculateService(price, weight);
+        System.out.print("Размер пошлины в рублях составит: " + tax + " руб.");
     }
 }
--- homeWork2/src/ru/netology/service/CustomsService.java
+++ homeWork2/src/ru/netology/service/CustomsService.java
@@ -1,7 +1,8 @@
 package ru.netology.service;
 public class  CustomsService{
+    public static final int TAX = 100;
     public static int calculateService(int price, int weight){
-        int tax = price / 100 + weight * 100;
+        int tax = price / 100 + weight * TAX;
         return  tax;
     }
 }
